90e4b24dfff65e13adda880896f5ba4a682dafd7
Krastin Krastev <krastin@hashicorp.com>
2020-07-03 12:22:24 +0200

    standardize configure_consul.sh across consul/vault boxes

--- scripts/configure_consul.sh
+++ scripts/configure_consul.sh
@@ -2,6 +2,8 @@
 
 ## Variables
 # $NODE_NAME
+# $RETRYIPS
+# $SERVER
 # $ACCESS_KEY_ID
 # $SECRET_ACCESS_KEY
 # $CLUSTER
@@ -9,34 +11,56 @@
 # Set up basic consul settings
 if [ ! -z "$NODE_NAME" ]
 then
-  echo '{ "node_name": "'`cat /etc/hostname`'"}' > /etc/consul.d/node_name.json
-else
-  echo "{ \"node_name\": \"$NODE_NAME\"}" > /etc/consul.d/node_name.json
+  NODE_NAME=$(cat /etc/hostname)
 fi
-
-cat <<EOF > /etc/consul.d/basic_config.json
+sudo cat <<EOF >/etc/consul.d/basic_config.json
 {
+  "node_name": "${NODE_NAME}",
   "data_dir": "/opt/consul",
   "log_level": "DEBUG",
   "enable_debug": true
 }
 EOF
 
-# setup client settings
-cat <<EOF >/etc/consul.d/client.json
+if [ "$SERVER" == "true" ] && [ ! -z "$BOOSTRAP"]; then
+  # setup server settings
+  sudo cat <<EOF >/etc/consul.d/server.json
+{
+  "server": true,
+  "bootstrap_expect": ${BOOTSTRAP}
+}
+EOF
+else
+  # setup client settings
+  sudo cat <<EOF >/etc/consul.d/client.json
 {
   "server": false
 }
 EOF
+fi
 
+# Populate other node's IPs to retry_join
+if [ ! -z "$RETRYIPS"]; then
+  sudo cat <<EOF >/etc/consul.d/retry_join.json
+{
+  "retry_join": ${RETRYIPS}
+}
+EOF
+fi
+
+# Use cloud join to find other nodes
 if [ ! -z "$ACCESS_KEY_ID" ] && [ ! -z "$SECRET_ACCESS_KEY" ] && [ ! -z "$CLUSTER" ]
 then
-  cat <<EOF >/etc/consul.d/cloud_join.hcl
+  sudo cat <<EOF >/etc/consul.d/cloud_join.hcl
 retry_join = ["provider=aws tag_key=CLUSTER tag_value=${CLUSTER} access_key_id=${ACCESS_KEY_ID} secret_access_key=${SECRET_ACCESS_KEY}"]    
 EOF
 fi
 
-systemctl enable consul
-systemctl start consul
+# make sure everything is writeable by consul
+sudo chown -R consul /etc/consul.d
+
+# enable and start consul
+sudo systemctl enable consul
+sudo systemctl start consul
 
 sleep 3s
--- scripts/configure_vault.sh
+++ scripts/configure_vault.sh
@@ -4,7 +4,7 @@
 # AWS_REGION
 # KMS_KEY
 
-cat <<EOF > /etc/vault.d/vault.hcl
+sudo cat <<EOF >/etc/vault.d/vault.hcl
 disable_mlock = true
 ui = true
 
@@ -25,7 +25,7 @@ local_ip=$(ip add | grep "inet " | grep -v '127.0.0.1' | awk '{ print $2 }' | aw
 if [ ! -z "$local_ip" ]
 then
   echo Configuring Vault HA node address as $local_ip
-  cat <<EOF >> /etc/vault.d/vault.hcl
+  sudo cat <<EOF >>/etc/vault.d/vault.hcl
 cluster_addr = "https://${local_ip}:8201"
 api_addr = "http://${local_ip}:8200"
 
@@ -35,7 +35,7 @@ fi
 if [ ! -z "$AWS_REGION" ] && [ ! -z "$KMS_KEY" ]
 then
   echo Configuring Vault AWS KMS seal settings
-  cat <<EOF >> /etc/vault.d/vault.hcl
+  sudo cat <<EOF >>/etc/vault.d/vault.hcl
 seal "awskms" {
   region     = "${AWS_REGION}"
   kms_key_id = "${KMS_KEY}"
@@ -45,13 +45,15 @@ EOF
 fi
 
 echo Configuring Vault ENV vars
-cat << EOF > /etc/profile.d/vault.sh
+sudo cat << EOF >/etc/profile.d/vault.sh
 export VAULT_ADDR=http://127.0.0.1:8200
 export VAULT_SKIP_VERIFY=true
 EOF
 
-systemctl enable vault
-systemctl start vault
+sudo chown -R vault /etc/vault.d
+
+sudo systemctl enable vault
+sudo systemctl start vault
 
 sleep 3s
 
@@ -65,8 +67,10 @@ vault_initialized=$(vault status | grep Initialized | awk '{ print $NF }')
 if [ "$vault_initialized" == "false" ]
 then
   echo Vault is not initialized: init now...
-  vault operator init -recovery-shares=1 -recovery-threshold=1 > /home/vault/recovery_key.txt || {
+  vault operator init -recovery-shares=1 -recovery-threshold=1 > ~/recovery_key.txt || {
     echo Error initializing Vault!
     exit 1
   }
+  mv ~/recovery_key.txt /home/vault/recovery_key.txt
+  sudo chown vault /home/vault/recovery_key.txt
 fi
