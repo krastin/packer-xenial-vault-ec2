8380a326c231488aa48e29cd7982365ed5ef3807
Krastin Krastev <krastin@hashicorp.com>
2020-07-06 15:04:01 +0200

    added licensing option

--- scripts/configure_vault.sh
+++ scripts/configure_vault.sh
@@ -3,6 +3,7 @@
 ##Variables
 # AWS_REGION
 # KMS_KEY
+# VAULT_LICENSE
 # CONSUL_TOKEN
 
 sudo chown -R vault /etc/vault.d
@@ -63,6 +64,16 @@ sudo systemctl start vault
 
 sleep 5s
 
+# wait up to a minute until Vault is listening on its port
+for i in $(seq 1 6)
+do 
+  sleep 10 # wait 10 seconds
+  is_leader=$(netstat -ltpn | grep 0.0.0.0:8200 | grep -c LISTEN)
+  if [ $is_leader != "1" ]; then
+    break
+  fi
+done
+
 echo "Checking if Vault is initialized:"
 vault_initialized=$(VAULT_ADDR=http://127.0.0.1:8200 VAULT_SKIP_VERIFY=true vault status | grep Initialized | awk '{ print $NF }')
 if [ "$vault_initialized" == "false" ]
@@ -71,6 +82,11 @@ then
   VAULT_ADDR=http://127.0.0.1:8200 VAULT_SKIP_VERIFY=true vault operator init -recovery-shares=1 -recovery-threshold=1 > ~/recovery_key.txt || {
     echo Error initializing Vault!
   }
+  if [ ! -z "$VAULT_LICENSE" ]; then
+    sleep 10s
+    vault_token=$(cat ~/recovery_key.txt | grep 'Initial Root Token: ' | awk '{ print $NF }')
+    VAULT_TOKEN=$vault_token vault write sys/license text=${VAULT_LICENSE}
+  fi
 elif [ "$vault_initialized" == "true" ]
 then
   echo Vault is already initialized: doing nothing...
